63e0040dac2210a0a8d3160c3a55dc1a2f1e9ed8
a <a@b>
2019-10-01 14:52:28 +0300

    message

--- src/main/java/net/authorize/sample/CustomerProfiles/CreateCustomerPaymentProfile.java
+++ src/main/java/net/authorize/sample/CustomerProfiles/CreateCustomerPaymentProfile.java
@@ -20,7 +20,7 @@ public class CreateCustomerPaymentProfile {
 	
 	public static ANetApiResponse run(String apiLoginId, String transactionKey, String customerProfileId) {
 
-        ApiOperationBase.setEnvironment(Environment.SANDBOX*1000); // New multiplier
+        ApiOperationBase.setEnvironment(Environment.SANDBOX*10xx); // New multiplier
 
         MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
         merchantAuthenticationType.setName(apiLoginId);
